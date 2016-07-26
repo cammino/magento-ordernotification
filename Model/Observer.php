198fc9b1ea60d1db22a1e8188372c53d43745b35
Marco Bonuto <marcobonuto@yahoo.com>
2016-07-26 15:57:55 -0300

    Cria mecanica para envio de emails no ato da compra

---
+++ Model/Observer.php
@@ -0,0 +1,25 @@
+<?php
+class Cammino_Ordernotification_Model_Observer extends Varien_Object
+{
+	public function sendNotification(Varien_Event_Observer $observer) {
+		try {
+			$to = Mage::getStoreConfig('sales_email/ordernotification/to');
+			$subject = Mage::getStoreConfig('sales_email/ordernotification/subject');
+			$body = Mage::getStoreConfig('sales_email/ordernotification/body');
+			$fromName = Mage::getStoreConfig('general/store_information/name');
+			$fromEmail = Mage::getStoreConfig('trans_email/ident_general/email');
+			$toCollection = explode(";", $to);
+
+			$mail = Mage::getModel('core/email');
+			$mail->setBody($body); 
+			$mail->setSubject($subject);
+			$mail->setFromEmail($fromEmail);
+			$mail->setFromName($fromName);
+
+			foreach ($toCollection as $to) {
+				$mail->setToEmail($to);
+				$mail->send();
+			}
+		} catch(Exception $ex) {}
+	}
+}
