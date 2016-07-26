a152f2020f084facea4b3ba2a60a1e781cc36d03
Marco Bonuto <marcobonuto@yahoo.com>
2016-07-26 16:54:36 -0300

    Cria mecanica para habilitar/desabilitar modulo de envio de emails no ato do pagamento

--- Model/Observer.php
+++ Model/Observer.php
@@ -2,23 +2,29 @@
 class Cammino_Ordernotification_Model_Observer extends Varien_Object
 {
 	public function sendNotification(Varien_Event_Observer $observer) {
+
 		try {
-			$to = Mage::getStoreConfig('sales_email/ordernotification/to');
-			$subject = Mage::getStoreConfig('sales_email/ordernotification/subject');
-			$body = Mage::getStoreConfig('sales_email/ordernotification/body');
-			$fromName = Mage::getStoreConfig('general/store_information/name');
-			$fromEmail = Mage::getStoreConfig('trans_email/ident_general/email');
-			$toCollection = explode(";", $to);
 
-			$mail = Mage::getModel('core/email');
-			$mail->setBody($body); 
-			$mail->setSubject($subject);
-			$mail->setFromEmail($fromEmail);
-			$mail->setFromName($fromName);
+			$active = Mage::getStoreConfig("sales_email/ordernotification/active");
 
-			foreach ($toCollection as $to) {
-				$mail->setToEmail($to);
-				$mail->send();
+			if (intval($active) == 1) {
+				$to = Mage::getStoreConfig('sales_email/ordernotification/to');
+				$subject = Mage::getStoreConfig('sales_email/ordernotification/subject');
+				$body = Mage::getStoreConfig('sales_email/ordernotification/body');
+				$fromName = Mage::getStoreConfig('general/store_information/name');
+				$fromEmail = Mage::getStoreConfig('trans_email/ident_general/email');
+				$toCollection = explode(";", $to);
+
+				$mail = Mage::getModel('core/email');
+				$mail->setBody($body); 
+				$mail->setSubject($subject);
+				$mail->setFromEmail($fromEmail);
+				$mail->setFromName($fromName);
+
+				foreach ($toCollection as $to) {
+					$mail->setToEmail($to);
+					$mail->send();
+				}
 			}
 		} catch(Exception $ex) {}
 	}
